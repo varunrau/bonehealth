0ae8242a4087fae0e35451590e5bfbe4eacdf7b6
Charles Xue <charlesx2013@gmail.com>
2014-11-01 17:33:46 -0700

    added folders for styling

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 
 from sys import argv
-from bottle import route, run, template
+from bottle import route, run, template, get
 
 @route('/')
 def index():
     return "Hi, Varun!"
+
+@get("/<filename:re:.*\.js>")
+def javascripts(filename):
+    return static_file(filename, root="static/javascripts")
+
+@get("/<filename:re:.*\.(png|jpg)>")
+def images(filename):
+    return static_file(filename, root="static/images")
+
+@get("/<filename:re:.*\.css>")
+def stylesheets(filename):
+    return static_file(filename, root="static/stylesheets")
 
 run(host='0.0.0.0', port=argv[1])
