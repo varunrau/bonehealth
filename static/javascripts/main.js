094cf72b7cb7cd11dbdba702ce0b417530bb18bf
Charles Xue <charlesx2013@gmail.com>
2014-11-02 01:08:38 -0700

    moved function out

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -124,22 +124,15 @@ var PREGNANT = 'pregnant?'
 
 var user = new User();
 
-$(document).ready(function() {
-
-  // Switch between sections
-
-  function setSection(to, from)
-  {
-    if (!(to.hasClass("active")))
-    {  
-      from.animate({"left":"-100%"},100,'linear')
-      to.animate({"left":"0%"},100,'linear',function()
-      {    
-        from.css("left","100%");
-        from.removeClass("active");    
-        to.addClass("active");    
-      });
-    }
+function setSection(to, from) {
+  if (!(to.hasClass("active"))) {  
+    from.animate({"left":"-100%"},100,'linear')
+    to.animate({"left":"0%"},100,'linear',function()
+    {    
+      from.css("left","100%");
+      from.removeClass("active");    
+      to.addClass("active");    
+    });
   }
-});
+}
 
