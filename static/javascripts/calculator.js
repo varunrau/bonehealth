83158d5cbedee36ea2066ca3cebad01548ad915e
Charles Xue <charlesx2013@gmail.com>
2014-11-01 18:14:32 -0700

    javascripts

--- static/javascripts/calculator.js
+++ static/javascripts/calculator.js
@@ -1,3 +1,10 @@
-var calculor = function(extraCalcium) {
+$(document).ready(function() {
+  var user = {
+    age: 0,
+    sex: ''
+  }
 
-}
+  var calculator = function(extraCalcium) {
+    return extraCalcium * 300 + 300;
+  }
+});
