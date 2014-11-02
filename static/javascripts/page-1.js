796ab8da00d564b1986d2914a2b2dff145f5b966
Varun Rau <varunrau@gmail.com>
2014-11-02 07:12:00 -0800

    disable pregnancies

--- static/javascripts/page-1.js
+++ static/javascripts/page-1.js
@@ -14,10 +14,23 @@ $(document).ready(function() {
     var gender = $(this).html();
     if (gender == 'F') {
       $(this).html('M');
+      disablePregnancy();
     } else {
       $(this).html('F');
+      enablePregnancy();
     }
   });
+
+  function disablePregnancy() {
+    $('.pregnant-input').prop('disabled', function() { return true; })
+    $('.pregnant-input').prop('checked', false);
+    $('.switch').css('opacity', 0.5);
+  }
+
+  function enablePregnancy() {
+    $('.pregnant-input').prop('disabled', function() { return false; })
+    $('.switch').css('opacity', 1);
+  }
 
   $('#joke').click(function() {
     var joke = generateHumerusPuns();
